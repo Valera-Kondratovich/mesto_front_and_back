4ecc9ddca1d31eeab40f9c7ac34bd6cceb787fa2
Valera-Kondratovich <kondratovichval@yandex.ru>
2023-07-07 23:02:54 +0300

    первый коммит

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Слушаю порт 3001');
+  console.log('Слушаю порт 3000');
 });
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
