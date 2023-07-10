694a58b5f6a539eb223b11a6cfe36f49c74b3e93
Valera-Kondratovich <kondratovichval@yandex.ru>
2023-07-11 02:13:41 +0300

    ПР15

--- backend/app.js
+++ backend/app.js
@@ -31,17 +31,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 app.use(requestLogger);
+app.use(cors({
+  credentials: true,
+  origin: ['https://mesto.kondratovich.nomoreparties.sbs', 'http://mesto.kondratovich.nomoreparties.sbs'],
+}));
 //исправить потом на сервере
 app.use(cookieSession({
   secret: 'yourSecret',
   secure: false,
   httpOnly: false,
   sameSite: false,
-}));
-app.use(cors({
-  credentials: true,
-  origin: ['https://mesto.kondratovich.nomoreparties.sbs', 'http://mesto.kondratovich.nomoreparties.sbs'],
-}));
+}),
+);
 app.use(limiter);
 app.use(helmet());
 app.use(express.json());
