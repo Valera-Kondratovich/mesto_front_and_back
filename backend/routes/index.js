2adb36253704797f4cd520095aeb6086a4f8d4d8
Valera-Kondratovich <kondratovichval@yandex.ru>
2023-07-12 01:05:26 +0300

    ПР15

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,6 +10,11 @@ const {
   logout,
 } = require('../controllers/users');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
